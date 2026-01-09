4cc2f44c3ac8d74d6f2fc0710e686b6f46d45d0f
Onur <69401816+BenOnur@users.noreply.github.com>
2026-01-09 20:17:33 +0300

    Create Categories.java

---
+++ src/main/java/com/example/addon/Categories.java
@@ -0,0 +1,9 @@
+package com.example.addon;
+
+import meteordevelopment.meteorclient.systems.modules.Category;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+
+public class Categories {
+    public static final Category DONUT = new Category("DonutSMP", Items.COOKIE.getDefaultStack());
+}
